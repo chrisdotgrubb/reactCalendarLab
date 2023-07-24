63223d9ce41d711466c6b5e463c2af93f38564e9
chrisdotgrubb <developer.grubb@gmail.com>
2023-07-24 14:06:15 -0400

    Add ability to find day of week from day of month

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+	
+	const days = [
+		{
+			name: "Sunday"
+		},
+		{
+			name: "Monday"
+		},
+		{
+			name: "Tuesday"
+		},
+		{
+			name: "Wednesday"
+		},
+		{
+			name: "Thursday"
+		},
+		{
+			name: "Friday"
+		},
+		{
+			name: "Saturday"
+		},
+	]
+	
+	// The following creates an array of numbers from [1..28]
+	const dates = Array.from({length: 28}, (x, i) => i + 1)
+	
+	function getDayOfWeek(i) {
+				while ((i !== 6) && (Math.floor(i / 6))) {
+					i -= 7;
+				}
+				return days[i].name;
+			}
+			
+	const allDays = dates.map((d) => {
+			const dayOfWeek = getDayOfWeek(d - 1)
+			return (
+				<h6>{d} {dayOfWeek}</h6>
+			)
+		}
+	)
+	
+	return (
+		<div className="App">
+			<h1>React Calendar</h1>
+			<div className={'calendar'}>
+				{allDays}
+			</div>
+		</div>
+	);
 }
-
-export default App;
